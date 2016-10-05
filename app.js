8e3f0a0faed4609c68068c927024fd5e9a4892bc
Tim Horist Jr <Tim Horist Jr>
2016-10-05 16:32:10 -0700

    3-12_functions_strings

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', function ($scope) {
     
-    $scope.name = "Jane Doe";
-    $scope.occupation = 'Coder';
-
-    $scope.getName = function () {
-        return 'John Doe';
-    }
-
-    console.log($scope);
+    
 
 });
+
+var searchPeople = function (firstName, lastName, height, age, occupation) {
+    return 'Jane Doe';
+};
+
+var searchPeopleString = searchPeople.toString();
+
+console.log(searchPeople);
+console.log(searchPeopleString);
